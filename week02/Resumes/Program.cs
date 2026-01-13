d74f2bf7a996b3c8711efe7f324cdd8c47a91913
vikyrg05 <vikyrg05@gmail.com>
2026-01-13 09:15:19 -0400

    Add resume and job classes using abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job firstJob = new Job();
+        firstJob._jobTitle = "Software Engineer";
+        firstJob._company = "Microsoft";
+        firstJob._startYear = 2019;
+        firstJob._endYear = 2022;
+
+        Job secondJob = new Job();
+        secondJob._jobTitle = "Manager";
+        secondJob._company = "Apple";
+        secondJob._startYear = 2022;
+        secondJob._endYear = 2023;
+
+        Resume resume = new Resume();
+        resume._name = "Allison Rose";
+
+        resume._jobs.Add(firstJob);
+        resume._jobs.Add(secondJob);
+
+        resume.Display();
     }
 }
