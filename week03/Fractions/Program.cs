4479c161f51345b151587e6ab4c04057174413b5
vikyrg05 <vikyrg05@gmail.com>
2026-01-19 16:58:06 -0400

    Completed Fraction class with constructorss, getters, setters and methods

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+
+        Fraction f2 = new Fraction(5);
+
+        Fraction f3 = new Fraction(3, 4);
+
+        Fraction f4 = new Fraction(1, 3);
+
+        Console.WriteLine(f1.GetFractionString());  // "1/1"
+        Console.WriteLine(f1.GetDecimalValue());  // 1
+
+        Console.WriteLine(f2.GetFractionString());  // "5/1"
+        Console.WriteLine(f2.GetDecimalValue());    // 5
+
+        Console.WriteLine(f3.GetFractionString());   // "3/4"
+        Console.WriteLine(f3.GetDecimalValue());    // 0.75
+
+        Console.WriteLine(f4.GetFractionString());   // "1/3"
+        Console.WriteLine(f4.GetDecimalValue());    // 0.33333333333
+
     }
 }
