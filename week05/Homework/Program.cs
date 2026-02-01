493ebb1f9e4ad7f75c523c59661a1a705761dcce
vikyrg05 <vikyrg05@gmail.com>
2026-02-01 09:54:27 -0400

    Complete the Inheritance activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment myAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        string summary = myAssignment.GetSummary();
+        Console.WriteLine(summary);
+
+        MathAssignment mathAssignment = new MathAssignment(
+            "Roberto Rodriguez",
+
+            "Fractions",
+
+            "7.3",
+
+            "8-19"
+        );
+
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment(
+            "Mary Waters",
+            "European History",
+            "The Causes of World War II"
+        );
+
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
