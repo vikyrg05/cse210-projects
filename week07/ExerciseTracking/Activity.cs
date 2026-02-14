9da16bbce49beda5c9671c0ec9d854726cec0ebc
vikyrg05 <vikyrg05@gmail.com>
2026-02-14 16:46:14 -0400

    Complete Exercise Tracking Program

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,32 @@
+using System;
+
+public abstract class Activity
+{
+    private string _date;
+    private int _minutes;
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public string GetDate()
+    {
+        return _date;
+    }
+
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public string GetSummary()
+    {
+        return $"{_date} ({_minutes} min) - Distance: {GetDistance():F2} km, Speed: {GetSpeed():F2} kph, Pace: {GetPace():F2} min per km";
+    }
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        Running run = new Running("03 Nov 2022", 30, 5.0);
+        Cycling cycle = new Cycling("04 Nov 2022", 45, 20.0);
+        Swimming swim = new Swimming("05 Nov 2022", 40, 30);
+
+        activities.Add(run);
+        activities.Add(cycle);
+        activities.Add(swim);
+
+        double totalDistance = 0;
+        int totalMinutes = 0;
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+
+            totalDistance += activity.GetDistance();
+            totalMinutes += activity.GetMinutes();
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("----- Overall Summary -----");
+        Console.WriteLine($"Total Time: {totalMinutes} minutes");
+        Console.WriteLine($"Total Distance: {totalDistance:F2} km");
+        Console.WriteLine($"Average Speed: {(totalDistance / totalMinutes) * 60:F2} kph");
     }
 }
